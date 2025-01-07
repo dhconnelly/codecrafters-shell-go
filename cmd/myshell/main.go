d830f73aa7a0dd012e0e3bdff69e46b1aef6ef37
Daniel Connelly <daniel.connelly@airbnb.com>
2025-01-07 15:40:04 +0100

    command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,28 +8,54 @@ import (
 	"os"
 )
 
+var (
+	ErrCommandNotFound = errors.New("command not found")
+)
+
 type prompter struct {
 	prompt string
 	w      io.Writer
-	r      *bufio.Reader
+	s      *bufio.Scanner
 }
 
 func newPrompter(prompt string, w io.Writer, r io.Reader) *prompter {
-	return &prompter{prompt: prompt, w: w, r: bufio.NewReader(r)}
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+	return &prompter{prompt: prompt, w: w, s: s}
 }
 
 func (p *prompter) readline() (string, error) {
 	fmt.Fprint(p.w, p.prompt)
-	line, err := p.r.ReadString('\n')
-	if err != nil && !errors.Is(err, io.EOF) {
-		return "", err
+	if p.s.Scan() {
+		return p.s.Text(), nil
 	}
-	return line, nil
+	if p.s.Err() != nil {
+		return "", p.s.Err()
+	}
+	return "", io.EOF
+}
+
+type command interface{}
+
+func parse(line string) (command, error) {
+	return nil, fmt.Errorf("%s: %w", line, ErrCommandNotFound)
 }
 
 func main() {
 	p := newPrompter("$ ", os.Stdout, os.Stdin)
-	if _, err := p.readline(); err != nil {
-		panic(err)
+	for {
+		line, err := p.readline()
+		if err == io.EOF {
+			break
+		}
+		if line == "" {
+			continue
+		}
+		if err != nil {
+			panic(err)
+		}
+		if _, err := parse(line); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
 	}
 }
